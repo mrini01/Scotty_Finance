5aafbe1c1c44162760b68c1365d0e0fd65f97b34
TuanLenh <tuanlenh3085@gmail.com>
2024-02-01 18:06:30 -0800

    new change to welcome page

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('email-input').addEventListener('focus', function () {
-    document.getElementById('email-label').style.display = 'none';
-});
-
-document.getElementById('email-input').addEventListener('blur', function () {
-    if (this.value === '') {
-        document.getElementById('email-label').style.display = 'block';
-    }
-});
